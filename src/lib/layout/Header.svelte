9117b9ecd5d7f467a46170bcca1c893fe0703ba4
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2024-08-08 18:42:59 +0900

    feat: add frame (#3)
    * add frame
    
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>
    
    * feat: add frame
    
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>
    
    * remove: comment on Header component
    
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>
    
    ---------
    
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

---
+++ src/lib/layout/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import github from '$lib/images/github.svg';
+	import logo from '$lib/images/logo.png';
+</script>
+
+<header class="sticky">
+	<AppBar
+		class={'w-full'}
+		gridColumns="grid-cols-3"
+		slotDefault="place-self-center"
+		slotTrail="place-content-end"
+	>
+		<svelte:fragment slot="lead">
+			<a href="/">
+				<img src={logo} alt="Leginote" class="h-8 w-8" />
+			</a></svelte:fragment
+		>
+		<p class="h2">LegiNote</p>
+		<svelte:fragment slot="trail">
+			<a href="https://github.com/sveltejs/kit">
+				<img src={github} alt="GitHub" class="h-8 w-8" />
+			</a></svelte:fragment
+		>
+	</AppBar>
+</header>
---
+++ src/lib/layout/Sidebar.svelte
@@ -0,0 +1,3 @@
+<aside>
+	<nav></nav>
+</aside>
